ee6146458258d67f2eae19b8bb509e70bee1183b
Marcelo Carvalho <a21582@alunos.ipca.pt>
2021-03-17 15:11:26 +0000

    First test of OpenMP

---
+++ OpenMP_01/IPCA_PCA_OpenMP.cpp
@@ -0,0 +1,25 @@
+#include <omp.h>
+static long num_steps = 100000; 
+double step;
+
+#define NUM_THREADS 4
+int main() {
+	int i, nthreads; double pi, sum[NUM_THREADS];
+	step = 1.0 / (double)num_steps;
+	
+#pragma omp parallel num_threads(NUM_THREADS)
+	{
+		int i; 
+		double x;
+		int id = omp_get_thread_num();
+		int nthrds = omp_get_num_threads();
+		if (id == 0) nthreads = nthrds;
+		for (i = id, sum[id] = 0.0; i < num_steps; i = i + nthrds) {
+			x = (i + 0.5) * step;
+			sum[id] += 4.0 / (1.0 + x * x);
+		}
+	}
+
+	for (i = 0, pi = 0.0; i < nthreads; i++)
+		pi += sum[i] * step;
+}
